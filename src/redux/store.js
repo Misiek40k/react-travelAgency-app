e750065afe9d998d8e64c8fc6b4578dd58d70f06
Misiek40k <comanderrr@gmail.com>
2020-01-01 18:58:04 +0100

    Add OrderForm, OrderSummary and OrderOptions styles and Add pricing.json nad new utils

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
-import {combineReducers, createStore} from 'redux';
+import { combineReducers, createStore } from 'redux';
 import tripList from '../data/trips.json';
 
 import globalReducer from './globalRedux';
 import filtersReducer from './filtersRedux';
+import orderReducer from './orderRedux';
 
 // define initial state and shallow-merge initial data
 const initialState = {
@@ -11,6 +12,11 @@ const initialState = {
   regions: {},
   subregions: {},
   tags: {},
+  order: {
+    trip: null,
+    email: '',
+    options: {},
+  },
   filters: {
     searchPhrase: '',
     tags: [],
@@ -24,6 +30,7 @@ const initialState = {
 // define reducers
 const reducers = {
   filters: filtersReducer,
+  order: orderReducer,
 };
 
 // add blank reducers for initial state properties without reducers
