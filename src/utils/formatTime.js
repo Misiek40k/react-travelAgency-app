1e79a9cf1cb581a022a0ce5832827f2b281a4c2f
Misiek40k <comanderrr@gmail.com>
2020-02-08 21:25:22 +0100

    FIx formatTime and Add utils.test.js

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,1 +1,18 @@
-export const formatTime = () => `formatted time`;
+export const formatTime = countdown => {
+  if (countdown == null || isNaN(countdown) || countdown < 0) {
+    return null;
+  } else {
+    let sec, min, hour;
+    hour = Math.floor(countdown / (60 * 60)) + '';
+    hour = hour.padStart(2, '0');
+
+    min = Math.floor((countdown / 60) % 60) + '';
+
+    min = min.padStart(2, '0');
+
+    sec = (countdown % 60) + '';
+    sec = sec.padStart(2, '0');
+
+    return hour + ':' + min + ':' + sec;
+  }
+};
